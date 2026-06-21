b6c2a9c395de04f372672844d291cadabc9c9c90
lesgo-Beyond <sazedurrahman707@gmail.com>
2026-06-22 05:08:35 +0600

    skeleton ready

---
+++ items.js
@@ -0,0 +1,20 @@
+export const items = [
+    {
+        image: aestheticHome.jpeg,
+        name: Home,
+        description: [cool, sleek, calm],
+        price: 1000
+    }, 
+    {
+        image: aestheticBook.jpeg,
+        name: Book,
+        description: [light, warm, vivid],
+        price: 400
+    }, 
+    {
+        image: aestheticSoda.jpeg,
+        name: Soda,
+        description: [mint, pinch, hit],
+        price: 700
+    }, 
+]
